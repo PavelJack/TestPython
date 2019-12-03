79986d529a8d1676b24d1f5dfbeeb4bb3644cd71
PavelJack <jackalp@mail.ru>
2019-12-03 15:04:41 +0300

    New commit

--- newfile.py
+++ newfile.py
@@ -1,3 +1,10 @@
 import os
 
 print("New file")
+<<<<<<< refs/remotes/origin/NewBranch
+=======
+
+#Here is a start point for new branch
+#Second attempt ))
+>>>>>>> local
+#Ok. I'm here for the third time!
