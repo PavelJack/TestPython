74b7aa4f4e4cbd127468b5e9291ba4058b5b00c0
PavelJack <jackalp@mail.ru>
2019-12-03 15:07:06 +0300

    New commit 2

--- newfile.py
+++ newfile.py
@@ -1,6 +1,6 @@
 import os
 
-print("New file")
+print("New file merge? or not?")
 <<<<<<< refs/remotes/origin/NewBranch
 =======
 
@@ -8,3 +8,5 @@ print("New file")
 #Second attempt ))
 >>>>>>> local
 #Ok. I'm here for the third time!
+
+#Text for merge1!
